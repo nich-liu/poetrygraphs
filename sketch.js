799bb2139764a62542a004b293d965a3f5ff1f83
Nicholas Liu <laptoprabbit@yahoo.com>
2016-05-06 00:50:03 -0500

    Dynamic graph integration

--- sketch.js
+++ sketch.js
@@ -18,14 +18,10 @@ function setup() {
   sentenceInput = select('#sentence');
 
   // A button
-  var analyzeButton = select('#analyze')
+  var analyzeButton = select('#analyze');
   analyzeButton.mousePressed(analyze);
-
-  wordInput = select('#word');
-
-  // A button
-  var rhymeButton = select('#rhymes')
-  rhymeButton.mousePressed(rhyme);
+  var analyzeButton = select('#update');
+  analyzeButton.mousePressed(analyze);
 
   var clearButton = select('#clear');
   clearButton.mousePressed(clearAll);
@@ -76,12 +72,10 @@ function analyze() {
   var li5 = createElement('li', 'Syllable count: ' + syllableCount.join(' ')+ sentenceInput.value());
   li5.parent(ol);
 
-  //test for line breaks
-  var mystr=sentenceInput.value();
-mystr=nl2br(mystr);
+  //announce sum on click
 var sum = syllableCount.reduce((a, b) => a + b, 0);
 console.log(sum);
-alert(sum);
+alert("Graph generated, syllables="+sum);
 
 function nl2br (str, is_xhtml) {
      var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
@@ -91,23 +85,68 @@ function nl2br (str, is_xhtml) {
 // create  graph
 $("#visualization").html("");
 var container = document.getElementById('visualization');
-var items = [
-{x: '2014-06-11', y: sum},
-{x: '2014-06-12', y: 25},
-{x: '2014-06-13', y: 30},
-{x: '2014-06-14', y: 10},
-{x: '2014-06-15', y: 15},
-{x: '2014-06-16', y: 30}
-];
-
-var dataset = new vis.DataSet(items);
-var options = {
-start: '2014-06-10',
-end: '2014-06-18'
-};
-var graph2d = new vis.Graph2d(container, dataset, options);
+  var items = [
+      {x: '2014-06-11', y: sum, group: 0},
+      {x: '2014-06-12', y: 25, group: 0},
+      {x: '2014-06-13', y: 30, group: 0},
+      {x: '2014-06-14', y: -10, group: 0},
+      {x: '2014-06-15', y: 15, group: 0},
+      {x: '2014-06-16', y: 30, group: 0}
+  ];
+  var dataset = new vis.DataSet(items);
+  var options = {
+      start: '2014-06-10',
+      end: '2014-06-18',
+      dataAxis: {
+          showMinorLabels: false,
+          icons: true
+      }
+  };
+  var groupData = {
+      id: 0,
+      content: "Group Name",
+      options: {
+          drawPoints: {
+              style: 'square' // square, circle
+          },
+          shaded: {
+              orientation: 'zero' // top, bottom
+          }
+      }
+  };
+  var groups = new vis.DataSet();
+  groups.add(groupData);
+  var graph2d = new vis.Graph2d(container, dataset, groups, options);
+  updateStyle();
+  function updateStyle() {
+      groupData.style = "";
+      groupData.style += document.getElementById("color").value;
+      groupData.style += document.getElementById("line").value;
+      groupData.style += document.getElementById("thickness").value;
+      groupData.options.drawPoints = {};
+      groupData.options.drawPoints.styles = "";
+      groupData.options.drawPoints.style = document.getElementById("points").value;
+      groupData.options.drawPoints.styles += document.getElementById("pointcolor").value;
+      groupData.options.drawPoints.styles += document.getElementById("pointthickness").value;
+      groupData.options.drawPoints.styles += document.getElementById("pointfill").value;
+      groupData.options.drawPoints.size = Number(document.getElementById("pointsize").value);
+      if (groupData.options.drawPoints.style === "") {
+          groupData.options.drawPoints = false;
+      }
+      groupData.options.shaded = {};
+      groupData.options.shaded.style = "";
+      groupData.options.shaded.style += document.getElementById("fillcolor").value;
+      groupData.options.shaded.style += document.getElementById("fillopacity").value;
+      groupData.options.shaded.orientation = document.getElementById("fill").value;
+      if (groupData.options.shaded.orientation === "") {
+          groupData.options.shaded = false;
+      }
+      var groups = new vis.DataSet();
+      groups.add(groupData);
+      graph2d.setGroups(groups);
+  }
 }
-
+onclick=updateStyle();
 // Go through and remove all the divs
 function clearAll() {
   var par = selectAll('.text');
